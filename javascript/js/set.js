1f0971cc5b2ade0152e15dfb8e036de6c492f241
unknown <palsahab7268@gmail.com>
2021-12-04 20:40:44 +0530

    Bootstrapfiles

---
+++ javascript/js/set.js
@@ -0,0 +1,7 @@
+// document.write("hello");
+
+document.getElementById("a").innerHTML = "<center><h1>SUNIL PAL</h1></center>"
+document.getElementById("b").innerHTML="<p>Phone:9516667268</p>"
+document.getElementById("b").setAttribute("style","border:5px dotted red");
+// document.getElementById("b").removeAttribute("style");
+// document.getElementById("a")
